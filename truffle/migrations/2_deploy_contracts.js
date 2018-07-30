e036ceafb76b9969ba6b0946b7d96d3c10184f4a
niroshn <niroshanrd.13@cse.mrt.ac.lk>
2018-07-30 09:12:33 +0530

    Added : initail app

---
+++ truffle/migrations/2_deploy_contracts.js
@@ -0,0 +1,9 @@
+var ApprovalContract = artifacts.require("ApprovalContract");
+
+module.exports = function(deployer) {
+  deployer.deploy(ApprovalContract);
+}
+
+/*
+wish reform feature over razor butter crucial copy spend pelican wrestle armed
+*/
